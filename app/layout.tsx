01f4b3f7ceca56387d7a4b955a2b1cb16f2e1695
sananbsd2021 <89647979+sananbsd2021@users.noreply.github.com>
2025-01-03 22:14:35 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              
+              {/*
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
@@ -51,9 +51,9 @@ export default function RootLayout({
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>                
-              </nav>
+              </nav>  */}
 
-              {/* <Navbar /> */}
+               <Navbar /> 
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
@@ -62,7 +62,7 @@ export default function RootLayout({
                 <p>
                   Powered by{" "}
                   <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    href="/"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
